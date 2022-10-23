1b46dd339cbc3796507543a2ce8c8d3a2ba41260
Tucker Whitaker <trw2005.50@gmail.com>
2022-10-23 14:07:49 -0500

    basic layout for admin screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="NavBar">
+        <button className="NavButton">1</button>
+        <button className="NavButton">2</button>
+        <button className="NavButton">3</button>
+        <button className="NavButton">4</button>
+        <button className="NavButton">5</button>
+        <button className="NavButton">6</button>
+      </div>
+      <div className="Container">
+        <div className="List">
+          <div className="ListItem">placeholder</div>
+          <div className="ListItem">placeholder 1</div>
+          <div className="ListItem">placeholder 2</div>
+          <div className="ListItem">placeholder 3</div>
+        </div>
+        <div className="MainChart">
+          <table>
+            <tr>
+              <th>.</th>
+              <th>PlaceHolder</th>
+              <th>PlaceHolder</th>
+              <th>PlaceHolder</th>
+            </tr>
+            <tr>
+              <td>A</td>
+              <td>100%</td>
+              <td>100%</td>
+              <td>100%</td>
+            </tr>
+            <tr>
+              <td>B</td>
+              <td>100%</td>
+              <td>100%</td>
+              <td>100%</td>
+            </tr>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
