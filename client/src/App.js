963707c4e8cd74752e60235384fdb234a663625e
Tucker Whitaker <trw2005.50@gmail.com>
2022-10-25 12:40:28 -0500

    main page add column and row functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,52 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [NewName, SetNewName] = useState();
+
+  const AddRow = (text) => {
+    let child = document.createElement("tr");
+    child.id = "row" + document.getElementById("table").children.length;
+    console.log(child.id);
+    let child2 = document.createElement("td");
+    child2.innerHTML = text;
+    child.appendChild(child2);
+
+    for (
+      let i = 0;
+      i < document.getElementById("row0").children.length - 1;
+      i++
+    ) {
+      let cellparent = document.createElement("td");
+      let cell = document.createElement("input");
+      cell.className = "cell";
+      cellparent.appendChild(cell);
+      child.appendChild(cellparent);
+    }
+    document.getElementById("table").appendChild(child);
+  };
+
+  const AddColumn = (text) => {
+    let child2 = document.createElement("th");
+    child2.innerHTML = text;
+    document.getElementById("row0").appendChild(child2);
+
+    for (let i = 1; i < document.getElementById("table").children.length; i++) {
+      let cellparent = document.createElement("td");
+      let cell = document.createElement("input");
+      cell.className = "cell";
+      cellparent.appendChild(cell);
+      document.getElementById("row" + i).appendChild(cellparent);
+    }
+  };
+
+  const AddToList = (text) => {
+    let child = document.createElement("button");
+    child.className = "ListItem";
+    child.innerHTML = text;
+    document.getElementById("List").appendChild(child);
+  };
+
   return (
     <div className="App">
       <div className="NavBar">
@@ -12,31 +58,41 @@ function App() {
         <button className="NavButton">6</button>
       </div>
       <div className="Container">
-        <div className="List">
-          <div className="ListItem">placeholder</div>
-          <div className="ListItem">placeholder 1</div>
-          <div className="ListItem">placeholder 2</div>
-          <div className="ListItem">placeholder 3</div>
+        <div className="List" id="List">
+          <input
+            type="text"
+            placeholder="name"
+            onChange={(e) => {
+              SetNewName(e.target.value);
+            }}
+          ></input>
+          <button
+            onClick={() => {
+              AddToList(NewName);
+            }}
+          >
+            Add New
+          </button>
         </div>
+
         <div className="MainChart">
-          <table>
-            <tr>
+          <button
+            onClick={() => {
+              AddRow("text");
+            }}
+          >
+            AddRow
+          </button>
+          <button
+            onClick={() => {
+              AddColumn("text");
+            }}
+          >
+            AddColumn
+          </button>
+          <table id="table">
+            <tr id="row0">
               <th>.</th>
-              <th>PlaceHolder</th>
-              <th>PlaceHolder</th>
-              <th>PlaceHolder</th>
-            </tr>
-            <tr>
-              <td>A</td>
-              <td>100%</td>
-              <td>100%</td>
-              <td>100%</td>
-            </tr>
-            <tr>
-              <td>B</td>
-              <td>100%</td>
-              <td>100%</td>
-              <td>100%</td>
             </tr>
           </table>
         </div>
--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,6 @@ import TeacherLogin from "./pages/teacherlogin";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <TeacherLogin />
+    <App />
   </React.StrictMode>
 );
